beff3a1789785c18b2215e3ff5be50ada83f59bd
David Goss <dgoss@whiteclarkegroup.com>
2015-08-25 10:54:17 +0100

    Adding grunt task to create zip for chrome web store

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        compress: {
+            chrome: {
+                options: {
+                    mode: "zip",
+                    archive: "../inspector-clouseau.zip",
+                    pretty: true
+                },
+                files: [{
+                    expand: true,
+                    src: ["manifest.json", "icon.png", "devtools.html", "devtools.js"]
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-compress");
+
+    grunt.registerTask("default", ["build"]);
+    grunt.registerTask("build", ["compress:chrome"]);
+};
