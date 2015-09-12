3514b0b8052fd4d6a834a295f2fe3d957cb6116b
David Goss <david@davidgoss.co.uk>
2015-09-12 23:36:47 +0100

    Adding support for framesets, fixes #1

--- devtools.js
+++ devtools.js
@@ -1,15 +1,38 @@
 "use strict";
 chrome.devtools.panels.elements.createSidebarPane("Widget", function(sidebar) {
     let dijitQuery = `(function() {
-        if ("dijit" in window && "byNode" in dijit) {
-            return dijit.byNode($0) || dijit.getEnclosingWidget($0) || {result: "No widget detected"};
+        "use strict";
+
+        function tryDijit(win) {
+            if ("dijit" in win && "byNode" in win.dijit) {
+                return win.dijit.byNode($0) || win.dijit.getEnclosingWidget($0) || false;
+            }
+            return false;
         }
-        return {result: "Dijit not available"};
+
+        function scanWindow(win) {
+            let result;
+
+            if (win.frames.length) {
+                for (let i = 0; i < win.frames.length; i++) {
+                    result = tryDijit(win.frames[i]);
+                    if (result) {
+                        return result;
+                    }
+                }g
+            } else {
+                result = tryDijit(win);
+            }
+
+            return result || "No widget found or Dijit not present";
+        }
+
+        return scanWindow(window);
     }());`;
 
     function updateWidgetPanel() {
         sidebar.setExpression(dijitQuery);
-    };
+    }
 
     chrome.devtools.panels.elements.onSelectionChanged.addListener(updateWidgetPanel);
     updateWidgetPanel();
